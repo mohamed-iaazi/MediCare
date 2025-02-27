295d686cc7d0634a1da5ab738d8769ea8717199a
mohamed <mohamed@gmail.com>
2025-02-27 17:42:33 +0000

    i fix database sql query

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,26 +1,25 @@
-create  database docotrdb;
+create  table  User (
+                        id int  primary key auto_increment ,
+                        username varchar(200),
+                        email  varchar(200),
+                        password  varchar (200),
+                        numberPhone varchar(250)
 
-use doctordb;
 
-
-    create  table  User (
-        id int  primary key auto_increment ,
-        username varchar(200),
-        email  varchar(200),
-        password  varchar (200),
-        numberPhone varchar(250)
-
-    )
-
+);
 
 create  table  Docotr (
-                        id int  primary key  ,
-                        specialisation varchar(250)
+                          DoctorId int primary key auto_increment,
 
-)
-
-create  table  Patient (
-                          id int primary key   ,
+                          id int    ,
                           specialisation varchar(250)
 
-)
+);
+
+create  table  Patient (
+                           PatientId int primary key auto_increment,
+                           id int ,
+                           specialisation varchar(250),
+                           foreign key (id) references user(id)
+
+);
